8ec516b081376d3e03f54bfd25bf2030ef570dad
shawkyMoj <shawky.foda@gmail.com>
2024-07-28 14:45:23 +0300

    assume Both Database and application will run on the same vm instance either locally or on GCP

--- SMARTVALUE_CONFIG_HOME_GCP/smartTool/Batches/If_Data_Found_Send_Mail/20/Mailesender_22744_0.bat
+++
@@ -1,9 +0,0 @@
-call D:\SmartValue\Sources\SMARTVALUE_CONFIG_HOME\smartTool\Batches\If_Data_Found_Send_Mail\Mailesender_init.bat
-
-set _APP= Support.SqlReader ifDataFoundSendQueryResultByMail 22744 -1 "PNU_PROD" JCCS abc123 
-
-@echo on
-
-java.exe -classpath "%_CP%" %_OPT% %_APP% >> 22744_0.log
-
-@echo off
--- SMARTVALUE_CONFIG_HOME_GCP/smartTool/Batches/If_Data_Found_Send_Mail/Mailesender_templ.bat
+++
@@ -1,9 +0,0 @@
-call D:\SmartValue\Sources\SMARTVALUE_CONFIG_HOME\smartTool\Batches\If_Data_Found_Send_Mail\Mailesender_init.bat
-set _APP= Support.SqlReader ifDataFoundSendQueryResultByMail <QUERY_ID> -1 "<DB CONFIGURATION FILE CONNECTION NAME>" <USERNAME> <PASSWORD> 
-
-@echo on
-
-java.exe -classpath "%_CP%" %_OPT% %_APP% >> <OUT_LOG_FILE>
-
-@echo off
-
