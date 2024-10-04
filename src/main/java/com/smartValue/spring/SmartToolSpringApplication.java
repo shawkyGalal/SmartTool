f1fc2d66051719ed7552227a1630a40236d934f2
shawkyGalal <shawky.foda@gmail.com>
2024-10-04 04:11:56 +0300

    Start Enable Spring Boot

---
+++ src/main/java/com/smartValue/spring/SmartToolSpringApplication.java
@@ -0,0 +1,13 @@
+package com.smartValue.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartToolSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartToolSpringApplication.class, args);
+	}
+
+}
